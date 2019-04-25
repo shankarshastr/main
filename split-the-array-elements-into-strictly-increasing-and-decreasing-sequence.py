3b3576063bdc07b51ee9f6e28d38fcac53025e28
snarayan <shankarnarayanshastri@gmail.com>
2019-04-25 18:16:38 +0530

    Initial Draft

--- split-the-array-elements-into-strictly-increasing-and-decreasing-sequence.py
+++ split-the-array-elements-into-strictly-increasing-and-decreasing-sequence.py
@@ -1,6 +1,6 @@
 # This contains
-# a = [7, 2, 2, 2, 2, 3, 1, 4] #This is a case where it has more than two occurrences of same element in this case it should fail
-# a = [7, 2, 3, 1, 4]# This is one test case where all elements are distinct so one array should be empty
+# a = [7, 2, 2, 2, 2, 3, 1, 4] #TestCase -1 : This is a case where it has more than two occurrences of same element in this case it should fail
+# a = [7, 2, 3, 1, 4]# TestCase -2 : This is one test case where all elements are distinct so one array should be empty
 a = [7, 2, 3, 7, 3, 1, 4]
 
 a1 = []
